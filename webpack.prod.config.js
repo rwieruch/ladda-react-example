659f3f7e6bba85c14dea963e217ee308fbd30c9a
Robin Wieruch <wrobin@gmx.net>
2017-03-03 16:49:21 +0100

    add dist build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,11 +1,9 @@
 var webpack = require('webpack');
 
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/index.js'
-  ],
+  entry: {
+    main: './src/index.js'
+  },
   module: {
     loaders: [
       {
@@ -35,9 +33,14 @@ module.exports = {
     new webpack.ProvidePlugin({
       'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
     }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
     new webpack.DefinePlugin({
       'process.env': {
-         NODE_ENV: JSON.stringify('development')
+         NODE_ENV: JSON.stringify('production')
        }
     })
   ]
